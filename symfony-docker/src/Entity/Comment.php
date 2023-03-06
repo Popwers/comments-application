b52f4f55beeb871d34807521ea44d4126d2ac9c2
Popwers <lionel.bataille@hotmail.com>
2023-03-06 13:17:00 +0400

    Set checker

--- symfony-docker/src/Entity/Comment.php
+++ symfony-docker/src/Entity/Comment.php
@@ -5,105 +5,104 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
-class Comment
-{
+#[ORM\HasLifecycleCallbacks]
+class Comment {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank]
     private ?string $lastName = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotBlank]
     private ?\DateTimeInterface $birthDate = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
     private ?string $text = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    public function getId(): ?int
-    {
+    #[ORM\PrePersist]
+    public function setCreatedAtValue() {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getEmail(): ?string
-    {
+    public function getEmail(): ?string {
         return $this->email;
     }
 
-    public function setEmail(string $email): self
-    {
+    public function setEmail(string $email): self {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getFirstName(): ?string
-    {
+    public function getFirstName(): ?string {
         return $this->firstName;
     }
 
-    public function setFirstName(string $firstName): self
-    {
+    public function setFirstName(string $firstName): self {
         $this->firstName = $firstName;
 
         return $this;
     }
 
-    public function getLastName(): ?string
-    {
+    public function getLastName(): ?string {
         return $this->lastName;
     }
 
-    public function setLastName(string $lastName): self
-    {
+    public function setLastName(string $lastName): self {
         $this->lastName = $lastName;
 
         return $this;
     }
 
-    public function getBirthDate(): ?\DateTimeInterface
-    {
+    public function getBirthDate(): ?\DateTimeInterface {
         return $this->birthDate;
     }
 
-    public function setBirthDate(\DateTimeInterface $birthDate): self
-    {
+    public function setBirthDate(\DateTimeInterface $birthDate): self {
         $this->birthDate = $birthDate;
 
         return $this;
     }
 
-    public function getText(): ?string
-    {
+    public function getText(): ?string {
         return $this->text;
     }
 
-    public function setText(string $text): self
-    {
+    public function setText(string $text): self {
         $this->text = $text;
 
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
+    public function getCreatedAt(): ?\DateTimeImmutable {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
-    {
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self {
         $this->createdAt = $createdAt;
 
         return $this;

ce18de58c5a2e6143d95c2faae646ab612b3e1ae
Popwers <lionel.bataille@hotmail.com>
2023-03-07 11:40:43 +0400

    Create the app

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,30 +1,233 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+	import { ref, onMounted } from 'vue';
+	import VueDatePicker from '@vuepic/vue-datepicker';
+	import '@vuepic/vue-datepicker/dist/main.css';
+	import Comment from './components/comment.vue';
+
+	const darkMode = ref(false);
+	darkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	window
+		.matchMedia('(prefers-color-scheme: dark)')
+		.addEventListener('change', e => (darkMode.value = e.matches));
+
+	const comments = ref([] as any[]);
+	const formData = ref({
+		firstName: {
+			value: '',
+			error: false,
+			message: '',
+		},
+		lastName: {
+			value: '',
+			error: false,
+			message: '',
+		},
+		birthDate: {
+			value: new Date().toDateString(),
+			error: false,
+			message: '',
+		},
+		email: {
+			value: '',
+			error: false,
+			message: '',
+		},
+		comment: {
+			value: '',
+			error: false,
+			message: '',
+		},
+	});
+
+	const fetchData = async () => {
+		const request = await fetch('https://localhost:4443/api/comments');
+		const respond = await request.json();
+		comments.value = respond['hydra:member'].reverse();
+	};
+
+	const sendData = async () => {
+		const { email, firstName, lastName, birthDate, comment } = formData.value;
+
+		Object.values(formData.value).forEach(field => {
+			field.error = false;
+			field.message = '';
+		});
+
+		const request = await fetch('https://localhost:4443/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({
+				email: email.value,
+				firstName: firstName.value,
+				lastName: lastName.value,
+				birthDate: birthDate.value,
+				text: comment.value,
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		});
+
+		const respond = await request.json();
+
+		if (
+			respond['hydra:description'] ===
+			'The data is either an empty string or null, you should pass a string that can be parsed with the passed format or a valid DateTime string.'
+		) {
+			formData.value.birthDate.error = true;
+			formData.value.birthDate.message = 'Veuillez entrer une date valide';
+		}
+
+		if (respond.violations) {
+			respond.violations.forEach((violation: { propertyPath: string; message: string }) => {
+				switch (violation.propertyPath) {
+					case 'email':
+						formData.value.email.error = true;
+						formData.value.email.message = violation.message;
+						break;
+					case 'firstName':
+						formData.value.firstName.error = true;
+						formData.value.firstName.message = violation.message;
+						break;
+					case 'lastName':
+						formData.value.lastName.error = true;
+						formData.value.lastName.message = violation.message;
+						break;
+					case 'birthDate':
+						formData.value.birthDate.error = true;
+						formData.value.birthDate.message = violation.message;
+						break;
+					case 'text':
+						formData.value.comment.error = true;
+						formData.value.comment.message = violation.message;
+						break;
+					default:
+						console.log('Unknown error');
+				}
+			});
+		}
+
+		if (respond.id) {
+			formData.value.comment.value = '';
+			fetchData();
+		}
+	};
+
+	onMounted(fetchData);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<h1>Comments App</h1>
+
+	<form @submit.prevent="sendData">
+		<div class="form-group">
+			<div>
+				<label for="firstName">Prénom</label>
+				<input
+					id="firstName"
+					@click="formData.firstName.error = false"
+					:class="{ error: formData.firstName.error }"
+					v-model="formData.firstName.value"
+				/>
+				<p class="error-message" v-show="formData.firstName.error">
+					{{ formData.firstName.message }}
+				</p>
+			</div>
+			<div>
+				<label for="lastName">Nom</label>
+				<input
+					id="lastName"
+					@click="formData.lastName.error = false"
+					:class="{ error: formData.lastName.error }"
+					v-model="formData.lastName.value"
+				/>
+				<p class="error-message" v-show="formData.lastName.error">
+					{{ formData.lastName.message }}
+				</p>
+			</div>
+		</div>
+		<div class="form-group">
+			<div>
+				<label for="birthDate">Date de naissance</label>
+				<VueDatePicker
+					id="birthDate"
+					:class="{ error: formData.birthDate.error }"
+					v-model="formData.birthDate.value"
+					:enable-time-picker="false"
+					:dark="darkMode"
+					auto-apply
+				></VueDatePicker>
+				<p class="error-message" v-show="formData.birthDate.error">
+					{{ formData.birthDate.message }}
+				</p>
+			</div>
+			<div>
+				<label for="email">E-mail</label>
+				<input
+					id="email"
+					@click="formData.email.error = false"
+					:class="{ error: formData.email.error }"
+					v-model="formData.email.value"
+				/>
+				<p class="error-message" v-show="formData.email.error">
+					{{ formData.email.message }}
+				</p>
+			</div>
+		</div>
+
+		<label for="comment">Commentaire</label>
+		<textarea
+			id="comment"
+			@click="formData.comment.error = false"
+			:class="{ error: formData.comment.error }"
+			v-model="formData.comment.value"
+		/>
+		<p class="error-message" v-show="formData.comment.error">
+			{{ formData.comment.message }}
+		</p>
+
+		<button>Envoyer</button>
+	</form>
+
+	<div class="comment-wrapper">
+		<Comment v-for="comment in comments" :key="comment.id" :comment="comment" />
+	</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+	h1 {
+		text-align: center;
+	}
+
+	form {
+		width: 60vw;
+		margin: 0 auto;
+		text-align: center;
+	}
+
+	label {
+		display: block;
+		margin: 0.5rem;
+	}
+
+	textarea {
+		display: block;
+		box-sizing: border-box;
+		width: 60vw;
+		min-height: 200px;
+		margin: 0.5rem auto;
+	}
+
+	.form-group {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.form-group div {
+		margin: 0.5rem;
+	}
+
+	.comment-wrapper {
+		margin-top: 2rem;
+	}
 </style>
---
+++ vue-app/src/components/comment.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+	const props = defineProps({ comment: Object });
+</script>
+
+<template>
+	<div class="card">
+		<div class="flex">
+			<h2>{{ props.comment?.firstName }} {{ props.comment?.lastName }}</h2>
+			<p class="date">
+				{{
+					new Date(Date.parse(props.comment?.birthDate)).toLocaleDateString('fr-FR', {
+						weekday: 'long',
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					})
+				}}
+			</p>
+		</div>
+		<p class="mail">{{ props.comment?.email }}</p>
+		<p>{{ props.comment?.text }}</p>
+	</div>
+</template>
+
+<style scoped>
+	h2 {
+		font-size: 1.5rem;
+		margin: 0;
+		margin-bottom: 0.2rem;
+	}
+
+	.date {
+		font-size: 0.8rem;
+		margin: 0;
+	}
+
+	.mail {
+		font-size: 0.7rem;
+		margin: 0;
+	}
+
+	.flex {
+		display: flex;
+		justify-content: space-between;
+	}
+	.card {
+		background-color: #fff;
+		border-radius: 0.5rem;
+		box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+		padding: 1rem;
+		margin-bottom: 1rem;
+	}
+</style>
